ffe1670a828bbb9d604eda63872dd4ad121208f6
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-15 20:31:23 -0500

    2019.11.15.08:30.PM

--- src/main/java/jbc/oct21/jindanupajit/blogapplication/controller/BlogEntryController.java
+++ src/main/java/jbc/oct21/jindanupajit/blogapplication/controller/BlogEntryController.java
@@ -7,14 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @Controller
+@RestController
 public class BlogEntryController {
 
     @Autowired
     BlogEntryRepository blogEntryRepository;
+
+
+    @GetMapping(value = "/rest/blog/retrieve", produces = "application/json")
+    public Iterable<BlogEntry> restBlogRetrieve() {
+        return blogEntryRepository.findAll();
+    }
 
     @GetMapping(value = {"/blog/view", "/"})
     public String pageBlogView(Model model) {
---
+++ src/main/java/jbc/oct21/jindanupajit/blogapplication/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+ */
+package jbc.oct21.jindanupajit.blogapplication.repository;
